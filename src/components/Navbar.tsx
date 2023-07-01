9374581d84b8b37dfef427a8a19700f37fb19d24
Zach Hunn <zach.hunn@trongov.us>
2023-06-30 15:05:53 -1000

    added strapi to the project as the backend

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+
+
+export const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <Link href={'/'}>Home</Link>
+
+      </nav>
+    </header>
+  )
+}
