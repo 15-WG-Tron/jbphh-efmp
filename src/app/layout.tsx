221814b8b30441d8c6ae5ebce9aa8bfaf75f1a73
Zach Hunn <zach.hunn@trongov.us>
2023-07-05 19:32:08 -1000

    merged main into feat/page-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css';
+import { Inter } from 'next/font/google';
 import React from 'react';
 import AppProvider from '../components/AppProvider';
+import { Navbar } from '../components/Navbar';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next app',
@@ -10,7 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <AppProvider>{children}</AppProvider>
+      <AppProvider>
+        <body className='container'>
+          <Navbar />
+          {children}
+        </body>
+      </AppProvider>
     </html>
   );
 }
